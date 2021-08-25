2260604418e9c38722f6bd1613afa7dbe071b3ec
Giovane Bianchi Milani <80916741+giovanebmilani@users.noreply.github.com>
2021-08-25 18:40:38 -0300

    .

---
+++ Exercicio3_Cofrinho/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String args[]){
+        Cofrinho c = new Cofrinho();
+
+        c.inserirMoeda(new Moeda(NomeMoeda.UmReal));
+        c.inserirMoeda(new Moeda(NomeMoeda.CinquentaCentavos));
+        c.inserirMoeda(new Moeda(NomeMoeda.VinteCincoCentavos));
+        c.inserirMoeda(new Moeda(NomeMoeda.UmReal));
+        c.inserirMoeda(new Moeda(NomeMoeda.CincoCentavos));
+        c.inserirMoeda(new Moeda(NomeMoeda.CinquentaCentavos));
+        c.inserirMoeda(new Moeda(NomeMoeda.UmReal));
+        c.inserirMoeda(new Moeda(NomeMoeda.DezCentavos));
+        c.inserirMoeda(new Moeda(NomeMoeda.UmCentavo));
+        c.inserirMoeda(new Moeda(NomeMoeda.VinteCincoCentavos));
+        c.inserirMoeda(new Moeda(NomeMoeda.UmReal));
+
+        System.out.println("Quantidade de moedas inseridas: " + c.getQuantidadeMoedas());
+        System.out.println("Quantidade de moedas de 1 real: " + c.getQuantidadeMoedasTipo(NomeMoeda.UmReal));
+        System.out.println("Quantidade de moedas de 50 centavos: " + c.getQuantidadeMoedasTipo(NomeMoeda.CinquentaCentavos));
+        System.out.println("Valor total em centavos: " + c.getValorTotalCentavos());
+        System.out.println("Valor total em reais: " + c.getValorTotalReais());
+        System.out.println("Moeda retirada: " + c.removerMoeda());
+        System.out.println("Moeda retirada: " + c.removerMoeda());
+        System.out.println("Valor total em centavos: " + c.getValorTotalCentavos());
+    }
+}
--- Exercicio3_Cofrinho/Cofrinho.java
+++ Exercicio3_Cofrinho/Cofrinho.java
@@ -3,10 +3,68 @@ import java.util.ArrayList;
 
 public class Cofrinho {
     
-    private ArrayList<String> moedas;
+    private ArrayList<Moeda> moedas;
 
     public Cofrinho() {
-        this.moedas = new ArrayList<String>();
+        this.moedas = new ArrayList<Moeda>();
     }
 
+    public ArrayList<Moeda> getMoedas() {
+        return this.moedas;
+    }
+
+    public void inserirMoeda(Moeda moeda) {
+        /** 
+         * Insere uma moeda no cofre
+         */
+        this.moedas.add(moeda);
+    }
+
+    public Moeda removerMoeda() {
+        /** 
+         * Remove e retorna a última moeda inserida no cofre,
+         * caso esteja vazio, retorna null
+        */
+        if (this.moedas.isEmpty()) {
+            return null;
+        } else {
+            Moeda moedaRemovida = this.moedas.remove(-1);
+            return moedaRemovida;
+        }
+    }
+
+    public int getQuantidadeMoedas() {
+        /** Retorna a quantidade de moedas no cofre */
+        return this.moedas.size();
+    }
+
+    public int getQuantidadeMoedasTipo(NomeMoeda nomeMoeda) {
+        /** Retorna a quantidade de uma moeda especifica */
+        int quantidade = 0;
+        for (Moeda moeda : this.moedas) {
+            if (moeda.getNomeMoeda() == nomeMoeda) {
+                quantidade++;
+            }
+        }
+        return quantidade;
+    }
+
+    public int getValorTotalCentavos() {
+        /** Retorna o valor total contido no cofre em centavos */
+        int centavos = 0;
+        for (Moeda moeda : this.moedas) {
+            centavos += moeda.getValorCentavos();
+        }
+        return centavos;
+    }
+
+    public double getValorTotalReais() {
+        /** Retorna o valor total contido no cofre em reais*/
+        double reais = 0;
+        for (Moeda moeda : this.moedas) {
+            reais += moeda.getValorReais();
+        }
+        return reais;
+    }
+ 
 }
