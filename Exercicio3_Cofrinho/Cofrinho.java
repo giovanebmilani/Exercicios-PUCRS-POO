85e3048429a85859b7454e5155cd934631d2a150
Giovane Bianchi Milani <80916741+giovanebmilani@users.noreply.github.com>
2021-08-25 18:00:27 -0300

    Exercicio 3

---
+++ Exercicio3_Cofrinho/Cofrinho.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+
+public class Cofrinho {
+    
+    private ArrayList<String> moedas;
+
+    public Cofrinho() {
+        this.moedas = new ArrayList<String>();
+    }
+
+}
