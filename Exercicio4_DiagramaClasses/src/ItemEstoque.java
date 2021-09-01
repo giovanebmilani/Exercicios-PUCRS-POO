08460cce32be0fdcec8d1520742d78a77d941a51
Giovane Bianchi Milani <80916741+giovanebmilani@users.noreply.github.com>
2021-09-01 20:38:05 -0300

    Objeto ItemEstoque

--- Exercicio4_DiagramaClasses/src/ItemEstoque.java
+++ Exercicio4_DiagramaClasses/src/ItemEstoque.java
@@ -1,3 +1,46 @@
+
+
 public class ItemEstoque {
     
+    private Produto produto;
+    private int quantidadeDisponivel;
+
+    public ItemEstoque(Produto produto, int quantidadeDisponivel) {
+        this.produto = produto;
+        this.quantidadeDisponivel = quantidadeDisponivel;
+    }
+
+    public Produto getProduto() {
+        return this.produto;
+    }
+    
+    public int getQuantidadeDisponivel() {
+        return this.quantidadeDisponivel;
+    }
+    
+    public boolean baixaEstoque(int qtd) {
+        if (qtd < 0) {
+            return false;
+        }
+
+        if (qtd > this.quantidadeDisponivel) {
+            return false;
+        }
+
+        this.quantidadeDisponivel -= qtd;
+        return true;
+    }
+
+    public boolean reposicaoEstoque(int qtd) {
+        if (qtd < 0) {
+            return false;
+        }
+
+        this.quantidadeDisponivel += qtd;
+        return true;
+    }
+ 
+    public boolean removeProduto(int codigo) {
+        return true;
+    }
 }
