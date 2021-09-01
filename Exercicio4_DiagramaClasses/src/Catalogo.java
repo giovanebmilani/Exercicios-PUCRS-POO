bd739f89b27e2e1f7232338acb065c162d2967d6
Giovane Bianchi Milani <80916741+giovanebmilani@users.noreply.github.com>
2021-09-01 20:16:05 -0300

    Objeto Catalogo

--- Exercicio4_DiagramaClasses/src/Catalogo.java
+++ Exercicio4_DiagramaClasses/src/Catalogo.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+
 public class Catalogo {
     
+    private ArrayList<Produto> produtos;
+
+    public Catalogo() {
+        this.produtos = new ArrayList<Produto>();
+    }
+
+    public Produto getProduto (int codigo) {
+        for (Produto produto : this.produtos) {
+            if (produto.getCodigo() == codigo) {
+                return produto;
+            }
+        }
+        return null;
+    }
+    
+    public boolean cadastra(Produto produto) {
+        if (produto == null) {
+            return false;
+        }
+        return produtos.add(produto);
+    }
+ 
 }
