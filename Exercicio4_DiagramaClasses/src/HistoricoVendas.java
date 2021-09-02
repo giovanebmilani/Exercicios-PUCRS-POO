d5b0f0f76519a424b12121c7e7f8df0fafcaeeaf
Giovane Bianchi Milani <80916741+giovanebmilani@users.noreply.github.com>
2021-09-01 22:13:00 -0300

    Estrutura objeto HistoricoVendas

--- Exercicio4_DiagramaClasses/src/HistoricoVendas.java
+++ Exercicio4_DiagramaClasses/src/HistoricoVendas.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+
 public class HistoricoVendas {
     
+    private ArrayList<Venda> vendas;
+
+    public HistoricoVendas() {
+        this.vendas = new ArrayList<Venda>();
+    }
+
+    public Venda getVenda() {
+        return null;
+    }
+
+    public boolean addVenda(Venda venda) {
+        return true;
+    }
+
+    public ArrayList<Venda> getUltimasVendas() {
+        return null;
+    }
+ 
 }
