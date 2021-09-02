b807f5912de4fceb4ef829018a1912bb2c274048
Giovane Bianchi Milani <80916741+giovanebmilani@users.noreply.github.com>
2021-09-01 22:02:04 -0300

    Estrutura objeto ItemVenda

--- Exercicio4_DiagramaClasses/src/ItemVenda.java
+++ Exercicio4_DiagramaClasses/src/ItemVenda.java
@@ -1,3 +1,37 @@
+
+
 public class ItemVenda {
+
+    private int numero;
+    private Produto produto;
+    private int quantidade;
+    private double precoUnitarioCobrado;
     
+    public ItemVenda(int numero, Produto produto, int quantidade, double precoUnitarioCobrado) {
+        this.numero = numero;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.precoUnitarioCobrado = precoUnitarioCobrado;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public Produto getProduto() {
+        return this.produto;
+    }
+
+    public int getQuantidade() {
+        return this.quantidade;
+    }
+
+    public double getPrecoUnitarioCobrado() {
+        return this.precoUnitarioCobrado;
+    }
+
+    public double getValorItem() {
+        return 0.0;
+    }
+
 }
