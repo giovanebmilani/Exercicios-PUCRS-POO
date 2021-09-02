5e49283c305b4eb3339584cee07c65bfe8a235b5
Giovane Bianchi Milani <80916741+giovanebmilani@users.noreply.github.com>
2021-09-01 21:53:47 -0300

    Estrutura objeto Estoque

--- Exercicio4_DiagramaClasses/src/Estoque.java
+++ Exercicio4_DiagramaClasses/src/Estoque.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
+
 public class Estoque {
     
+    private ArrayList<ItemEstoque> items;
+
+    public Estoque() {
+        this.items = new ArrayList<ItemEstoque>();
+    }
+
+    public int getQuantidadeDisponivel(int codigo) {
+
+        return 0;
+    }
+
+    public boolean baixaEstoque(int codigo, int qtd) {
+        return true;
+    }
+
+    public boolean adicionaProduto(Produto produto, int qtd) {
+        return true;
+    }
+    
+
 }
