9883abea1c29b51f66a9f7e1e74cd9659729c364
Giovane Bianchi Milani <80916741+giovanebmilani@users.noreply.github.com>
2021-09-03 10:06:06 -0300

    Objeto Estoque

--- Exercicio4_DiagramaClasses/src/Estoque.java
+++ Exercicio4_DiagramaClasses/src/Estoque.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 
 public class Estoque {
     
-    private ArrayList<ItemEstoque> items;
+    private ArrayList<ItemEstoque> itens;
 
     public Estoque() {
-        this.items = new ArrayList<ItemEstoque>();
+        this.itens = new ArrayList<ItemEstoque>();
     }
 
     public int getQuantidadeDisponivel(int codigo) {
-        for (ItemEstoque item : items) {
+        for (ItemEstoque item : itens) {
             if (item.getProduto().getCodigo() == codigo) {
                 return item.getQuantidadeDisponivel();
             }
@@ -17,17 +17,43 @@ public class Estoque {
         return -1;
     }
 
-    public boolean baixaEstoque(int codigo, int qtd) {
-        if (qtd < 0) {
-            return false;
+    public ItemEstoque getItem(int codigo) {
+        for (ItemEstoque item: this.itens) { 
+            if (item.getProduto().getCodigo() == codigo) {
+                return item;
+            }
         }
+        return null;
+    }
 
-        return true;
+    public boolean baixaEstoque(int codigo, int qtd) {
+        ItemEstoque item = getItem(codigo);
+        if (item == null)
+            return false;
+        return item.baixaEstoque(qtd);
     }
 
     public boolean adicionaProduto(Produto produto, int qtd) {
-        return true;
+        if (produto == null) 
+            return false;
+
+        if (qtd < 0)
+            return false;
+
+        ItemEstoque item = new ItemEstoque(produto, qtd);
+        // Verifica se o produto ja existe
+        if (jaExiste(item))
+            return false;
+
+        return itens.add(item);
     }
     
+    public boolean jaExiste(ItemEstoque item) {
+        for (ItemEstoque itemEstoque : this.itens) {
+            if (itemEstoque == item)
+                return true;
+        }
+        return false;
+    }
 
 }
