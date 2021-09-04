1af20302e36a3acb96f5cbae19a0e19a9bb99c29
Giovane Bianchi Milani <80916741+giovanebmilani@users.noreply.github.com>
2021-09-04 18:07:18 -0300

    Catalogo naão adiciona mais produtos ja existentes

--- Exercicio4_DiagramaClasses/src/Catalogo.java
+++ Exercicio4_DiagramaClasses/src/Catalogo.java
@@ -10,18 +10,25 @@ public class Catalogo {
 
     public Produto getProduto (int codigo) {
         for (Produto produto : this.produtos) {
-            if (produto.getCodigo() == codigo) {
+            if (produto.getCodigo() == codigo)
                 return produto;
-            }
         }
         return null;
     }
     
     public boolean cadastra(Produto produto) {
-        if (produto == null) {
+        if (produto == null) 
             return false;
-        }
+        if (jaExiste(produto))
+            return false ;
         return produtos.add(produto);
     }
  
+    public boolean jaExiste(Produto produto) {
+        for (Produto produtoCatalogo : this.produtos) {
+            if (produtoCatalogo.getCodigo() == produto.getCodigo())
+                return true;
+        }
+        return false;
+    }
 }
